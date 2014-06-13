262c617232d13f72bed97c97faeb5faf98d697b8
milez770 <milez770@gmail.com>
2014-06-13 22:47:51 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,18 +1,32 @@
-var scene, camera, renderer, light, controls, ray;
+var scene, camera, renderer, light, controls, ray, counter, blood;
 var keys=[];
+var truth = '';
 
+var playerMesh;
+var dummies = {};
+var objects = [];
+var fiveRoom = new THREE.BoxGeometry(25,25,25);
+var hRoom = new THREE.BoxGeometry(100,100,100);
+var tenRoom = new THREE.BoxGeometry(50,50,50);
+var hallway = new THREE.BoxGeometry(10,50,100);
+var bathRoom = new THREE.BoxGeometry(15,25,25);
+var flatGeo = new THREE.PlaneGeometry(20,20);
+
+var redMat = new THREE.MeshLambertMaterial({color:0xff0000, side:THREE.DoubleSide});
+var grayMat = new THREE.MeshLambertMaterial({color:0xffffff, side:THREE.DoubleSide});
+var greenMat = new THREE.MeshLambertMaterial({color:0x03e3e0, side:THREE.DoubleSide});
+var yellowMat = new THREE.MeshLambertMaterial({color:0xaaf100 , side:THREE.DoubleSide});
+var blueMat = new THREE.MeshLambertMaterial({color:0x00a1f3, side:THREE.DoubleSide});
+var oMat = new THREE.MeshLambertMaterial({color:0xe2d302, side:THREE.DoubleSide});
+var purMat = new THREE.MeshLambertMaterial({color:0x4e0663, side:THREE.DoubleSide});
+var pinkMat = new THREE.MeshLambertMaterial({color:0xfb86c7, side:THREE.DoubleSide});
+var pDouble = new THREE.MeshLambertMaterial({side:THREE.DoubleSide});
 
 //mapObjects
-var objects = [];
-
-//player
-var playerG = new THREE.BoxGeometry(5,5,5);
-var playerM = new THREE.MeshNormalMaterial();
-var playerMesh = new THREE.Mesh(playerG,playerM);
+var mapobjects ;
 
 var blocker = document.getElementById( 'blocker' );
 var instructions = document.getElementById( 'instructions' );
-
 
 var loopState;
 var enterances = [];
@@ -21,7 +35,8 @@ var velocityP = 0; //past velocity
 
 //init
 function init(){
-    
+
+   
     camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,1,1000);
     
     scene = new THREE.Scene();
@@ -36,16 +51,30 @@ function init(){
     ray = new THREE.Raycaster();
     ray.ray.direction.set( 0, -1,0);
     
-    
+    blood = new THREE.Object3D();
+	mapobjects = new THREE.Object3D();
+	
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth-2,window.innerHeight-2);
     document.body.appendChild(renderer.domElement);
    
+    //player
+    var playerG = new THREE.BoxGeometry(5,5,5);
+    var playerM = new THREE.MeshNormalMaterial();
+    playerMesh = new THREE.Mesh(playerG,playerM);
+
     controls.initiateF();
     mapMake();
+
     loopState = true;
+
+    //bloodmar
+
     scene.add(playerMesh);
-	
+
+
+	  
+    counter++;
         
 }
 
@@ -136,74 +165,43 @@ if ( havePointerLock ) {
 
 }
 
-//map loop
-function loopP(){
-	if( controls.getObjects().position.z <= -173 && controls.getObjects().position.x > 29){
-			controls.getObjects().position.x = 0;
-			controls.getObjects().position.z = 100;
-		}
-		
-		if(loopState){
-			if(controls.getObjects().position.z > 122){
-				velocity.z =0;
-				controls.getObjects().position.z = 122;
-			}
-		}
-}
-
-var fiveRoom = new THREE.BoxGeometry(25,25,25);
-var tenRoom = new THREE.BoxGeometry(50,50,50);
-var hallway = new THREE.BoxGeometry(10,50,100);
-var bathRoom = new THREE.BoxGeometry(15,25,25);
-var flatGeo = new THREE.PlaneGeometry(20,20);
-
-var redMat = new THREE.MeshLambertMaterial({color:0xff0000, side:THREE.DoubleSide});
-var grayMat = new THREE.MeshLambertMaterial({color:0xffffff, side:THREE.DoubleSide});
-var greenMat = new THREE.MeshLambertMaterial({color:0x03e3e0, side:THREE.DoubleSide});
-var yellowMat = new THREE.MeshLambertMaterial({color:0xaaf100 , side:THREE.DoubleSide});
-var blueMat = new THREE.MeshLambertMaterial({color:0x00a1f3, side:THREE.DoubleSide});
-var pDouble = new THREE.MeshLambertMaterial({side:THREE.DoubleSide});
-
-
 init();
 
 
 //requestAnim
 function draw(){
-   requestAnimationFrame(draw);
-//    checkKey();
- 	loopPlayer();
+
+  	requestAnimationFrame(draw);
+
+    loopPlayer();
   	colCheck();
-    controls.isOnObject( false );
+	
+//     controls.isOnObject( false );
 
-    ray.ray.origin.copy( controls.getObject().position );
+//     ray.ray.origin.copy( controls.getObject().position );
 
-    var intersections = ray.intersectObjects( objects );
+//     var intersections = ray.intersectObjects( objects );
 
-    if ( intersections.length > 0 ) {
+//     if ( intersections.length > 0 ) {
 
-        var distance = intersections[ 0 ].distance;
+//         var distance = intersections[ 0 ].distance;
 
-        if ( distance > 0 && distance < 10 ) {
+//         if ( distance > 0 && distance < 10 ) {
 
-            controls.isOnObject( true );
+//             controls.isOnObject( true );
 
-        }
+//         }
 
-    }
-   
-   if(ray.ray.origin.z == -171 && ray.ray.origin.x > 30){
-       ray.ray.position.x = 0;
-       ray.ray.position.z = 171;
-   }
+//     }
    
    controls.update();
    
    
-   light.position = ray.ray.origin;
+   light.position = controls.getObject().position;
+
    renderer.render(scene,camera);
    
-}
+};
 
 
 
@@ -211,64 +209,88 @@ draw();
 
 //map make
 function mapMake(){
-       var MeshRoom1 = new THREE.Mesh(tenRoom,redMat);     
-       var MeshRoom2 = new THREE.Mesh(tenRoom,blueMat);
-       var MeshRoom3 = new THREE.Mesh(fiveRoom,greenMat);
-       var MeshRoom4 = new THREE.Mesh(tenRoom,yellowMat);
-       var Meshbath = new THREE.Mesh(bathRoom,blueMat);
-       var MeshHallway1 = new THREE.Mesh(hallway,grayMat);
-       var MeshHallway2 = new THREE.Mesh(hallway,grayMat);
+     var MeshRoom1 = new THREE.Mesh(tenRoom,redMat);     
+     var MeshRoom2 = new THREE.Mesh(tenRoom,blueMat);
+     var MeshRoom3 = new THREE.Mesh(fiveRoom,greenMat);
+     var MeshRoom4 = new THREE.Mesh(tenRoom,yellowMat);
+     var MeshRoom5 = new THREE.Mesh(hRoom,blueMat);
+	   var MeshRoom6 = new THREE.Mesh(fiveRoom,purMat);
+	   var MeshRoom7 = new THREE.Mesh(tenRoom,yellowMat);
+	   var MeshRoom8 = new THREE.Mesh(tenRoom,pinkMat);
+	   
+	   
+     var Meshbath = new THREE.Mesh(bathRoom,blueMat);
+     var MeshHallway1 = new THREE.Mesh(hallway,grayMat);
+     var MeshHallway2 = new THREE.Mesh(hallway,grayMat);
+     
        
-       scene.add(MeshRoom1);
-       scene.add(MeshRoom2);
-       scene.add(MeshRoom3);
-       scene.add(MeshRoom4);
-       scene.add(Meshbath);
-       scene.add(MeshHallway1);
-       scene.add(MeshHallway2);
-       
-       MeshRoom1.position.y = 31.7;
-       
-       MeshRoom2.position.x = 50;
-       MeshRoom2.position.y = 31.7;
-       
-       MeshRoom3.position.x = 87.5;
-       MeshRoom3.position.z = -57.5;
-       MeshRoom3.position.y = 19;
-       
-       MeshRoom4.position.x = 50;
-       MeshRoom4.position.z = -50;
-       MeshRoom4.position.y = 31.7;
-       
-       MeshHallway1.position.y = 31.7;
-       MeshHallway1.position.z = 75;
-       
-       MeshHallway2.position.y = 31.7;
-       MeshHallway2.position.z = -125;
-       MeshHallway2.position.x = 31;
-       
-       Meshbath.position.y = 18.7;
-       Meshbath.position.z = -37.5;
-       
-       objects.push(MeshRoom1);
-       objects.push(MeshRoom2);
-       objects.push(MeshRoom3);
-       objects.push(MeshRoom4);
-       objects.push(Meshbath);
-       objects.push(MeshHallway1);
-       objects.push(MeshHallway1);
+     MeshRoom1.position.y = 31.7;
+     
+     MeshRoom2.position.x = 50;
+     MeshRoom2.position.y = 31.7;
+     
+     MeshRoom3.position.x = 62.5;
+     MeshRoom3.position.z = -87.5;;
+     MeshRoom3.position.y = 19;
+     
+     MeshRoom4.position.x = 50;
+     MeshRoom4.position.z = -50;
+     MeshRoom4.position.y = 31.7;
+	   
+	   MeshRoom5.position.y = 56.7;
+	   MeshRoom5.position.z = -125;
+	   
+	   MeshRoom6.position.x = -62.5;
+	   MeshRoom6.position.y = 19;
+	   MeshRoom6.position.z = -87.5;
 
+     MeshRoom7.position.x = -50;
+	   MeshRoom7.position.y = 31.7;
+	   MeshRoom7.position.z = -50;
+	   
+	   MeshRoom8.position.x = -50;
+	   MeshRoom8.position.y = 31.7;
+	   
+	   MeshHallway1.position.y = 31.7;
+     MeshHallway1.position.z = 75;
+     
+     MeshHallway2.position.y = 31.7;
+     MeshHallway2.position.z = -225;
+     
+     Meshbath.position.y = 18.7;
+     Meshbath.position.z = -37.5;
+     
+     objects.push(MeshRoom1);
+     objects.push(MeshRoom2);
+     objects.push(MeshRoom3);
+     objects.push(MeshRoom4);
+	 objects.push(MeshRoom5);
+	 objects.push(MeshRoom6);
+	 objects.push(MeshRoom7);
+	 objects.push(MeshRoom8);
+     objects.push(Meshbath);
+     objects.push(MeshHallway1);
+     objects.push(MeshHallway2);
+	 
+     mapobjects.add(MeshRoom1);
+     mapobjects.add(MeshRoom2);
+     mapobjects.add(MeshRoom3);
+     mapobjects.add(MeshRoom4);
+	 mapobjects.add(MeshRoom5);
+	 mapobjects.add(MeshRoom6);
+	 mapobjects.add(MeshRoom7);
+	 mapobjects.add(MeshRoom8);
+     mapobjects.add(Meshbath);
+     mapobjects.add(MeshHallway1);
+     mapobjects.add(MeshHallway2);
+	 
+	 scene.add(mapobjects)
+};
 
+function garbageMake(){
+	var gaGeo1 = new THREE.BoxGeometry(2,6,10);
+	var gaGeo2 = new 
 }
-
-
-
-// window.addEventListener("keydown",function(e){
-//     keys[e.keyCode]=true;
-// });
-// window.addEventListener("keyup",function(e){
-//     keys[e.keyCode]=false;
-// });
 
 //collision check
 function colCheck(){
@@ -294,33 +316,49 @@ function colCheck(){
 //collision
 function pushOut(){
 	
-	if(!colP){
-		velocityP = controls.velocity();
-		colP = true;
+	if()	
+		if(!colP){
+			velocityP = controls.velocity();
+			colP = true;
+		}
+		else if(colP){
+
+			if(velocityP.z < 0){
+				controls.velocity().z = Math.max(0,controls.velocity().z);
+			}
+			if(velocityP.z > 0){
+				controls.velocity().z = Math.min(0,controls.velocity().z);
+			}
+			if(velocityP.x > 0){
+				controls.velocity().x = Math.min(0,controls.velocity().x);
+			}
+			if(velocityP.x < 0){
+				controls.velocity().x = Math.max(0,controls.velocity().x);
+			}
+		}
+};
+
+
+function loopPlayer(){
+	
+	if(playerMesh.position.z <= -225  && playerMesh.position.x >=-2.5){
+		controls.getObject().position.x=0;
+		controls.getObject().position.z=120;
+		loopState = false;
+
+    var dummyG = new THREE.BoxGeometry(5,5,5);
+    var dummyM = new THREE.MeshNormalMaterial({color:0xf1e2d3});
+    var dummyP = new THREE.Mesh(dummyG, dummyM);
+    dummyP.position.y = 20;
+    scene.add(dummyP);
+    scene.add(blood);
+    console.log('added');
+
 	}
-	else if(colP){
-		
-		if(velocityP.z < 0){
-			controls.velocity().z = Math.max(0,controls.velocity().z);
-		}
-		if(velocityP.z > 0){
-			controls.velocity().z = Math.min(0,controls.velocity().z);
-		}
-		if(velocityP.x > 0){
-			
-			controls.velocity().x = Math.min(0,controls.velocity().x);
-		}
-		if(velocityP.x < 0){
-			controls.velocity().x = Math.max(0,controls.velocity().x);
+
+	if(playerMesh.position.z >123 && playerMesh.position.x<2.5){
+		if(loopState){
+			controls.getObject().position.z = 122.8;
 		}
 	}
 };
-
-function loopPlayer(){
-	if(loopState){
-		if(playerMesh.position.z <= -170 && playerMesh.position.x > 25){
-			controls.getObject().position.x=0;
-			controls.getObject().position.z=120;
-		}
-	}
-}
