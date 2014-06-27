6c810d910d49380150c3ddb40a122039d8984d66
milez770 <milez770@gmail.com>
2014-06-27 12:58:44 +0900

    Update PointerLockControls.js

--- js/controls/PointerLockControls.js
+++ js/controls/PointerLockControls.js
@@ -182,7 +182,7 @@ THREE.PointerLockControls = function ( camera ) {
 
 		if ( moveLeft ) velocity.x -= 200.0 * delta/5;
 		if ( moveRight ) velocity.x += 200.0 * delta/5;
-		if (run) velocity.x *= 1.001, velocity.z *= 1.001;
+		if (run) velocity.x *= 1.01, velocity.z *= 1.01;
 		
 		if ( isOnObject === true ) {
 
